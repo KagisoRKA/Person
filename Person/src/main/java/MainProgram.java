2924e9d5f278d53f339e4a8f5ae572b2d07b07ca
KagisoRKA <57135740+KagisoRKA@users.noreply.github.com>
2019-12-12 11:47:57 +0200

    Updating the file name as per project structure requirements

--- Person/src/main/java/MainProgram.java
+++ Person/src/main/java/MainProgram.java
@@ -1,4 +1,4 @@
-public class Main {
+public class MainProgram {
     public static void main(String[] args) {
         Person person = new Person("Joshua",26,"male",new String[] {"being a hardarse","agile","crazy","loving","ssd hard drives"});
         String greeting = person.hello();
